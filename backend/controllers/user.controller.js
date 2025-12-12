27a4736727a20ec60b1d9ba2af13bcdeea84f2fd
bytesourav-codebit <souravmondal.byte@gmail.com>
2025-12-12 16:05:45 +0530

    feat: add user login route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,9 +6,9 @@ module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
-  } 
+  }
 
-  console.log(req.body);
+  // console.log(req.body);
 
   const { fullname, email, password } = req.body;
 
@@ -26,4 +26,29 @@ module.exports.registerUser = async (req, res, next) => {
   const token = user.generateAuthToken();
 
   res.status(201).json({ token, user });
+};
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid email or password " });
+  }
+
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ error: "Invalid email or password " });
+  }
+
+  const token = user.generateAuthToken();
+
+  res.status(200).json({ token, user });  
 };
