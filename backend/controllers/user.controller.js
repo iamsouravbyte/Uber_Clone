1696ed13b210aa925ac10f941ab64bfb03d8450d
bytesourav-codebit <souravmondal.byte@gmail.com>
2025-12-12 15:35:18 +0530

    feat: add basic user registration route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,15 +6,19 @@ module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
-  }
+  } 
 
-  const { firstname, lastname, email, password } = req.body;
+  console.log(req.body);
+
+  const { fullname, email, password } = req.body;
 
   const hashPassword = await userModel.hashPassword(password);
 
   const user = await userService.createUser({
-    firstname,
-    lastname,
+    fullname: {
+      firstname: fullname.firstname,
+      lastname: fullname.lastname,
+    },
     email,
     password: hashPassword,
   });
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,24 +1,17 @@
 const userModel = require("../models/user.model");
 
-module.exports.createUser = async ({
-  firstname,
-  lastname,
-  email,
-  password,
-}) => {
-  if (!firstname || !email || !password) {
+module.exports.createUser = async ({ fullname, email, password }) => {
+  if (!fullname?.firstname || !email || !password) {
     throw new Error("All fields are required");
   }
 
-  const user = userModel.create({
+  const user = await userModel.create({
     fullname: {
-      fullname: {
-        firstname,
-        lastname,
-      },
-      email,
-      password,
+      firstname: fullname.firstname,
+      lastname: fullname.lastname,
     },
+    email,
+    password,
   });
 
   return user;
