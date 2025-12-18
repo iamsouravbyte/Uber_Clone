e889dbd5f1c4bffba84a49b67ce95aa4a75badae
bytesourav-codebit <souravmondal.byte@gmail.com>
2025-12-18 20:56:41 +0530

    feat: complete Captain signup UI and User Context file made

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,11 +1,109 @@
-import React from 'react'
+import React, { useState } from "react";
+import uberdriverlogo from "../assets/uber-driver-logo.png";
+import { Link } from "react-router-dom";
 
 const CaptainSignup = () => {
-  return (
-    <div>
-      CaptainSignup
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userData, setUserData] = useState("");
 
-export default CaptainSignup
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      fullName: {
+        firstName: firstName,
+        lastName: lastName,
+      },
+      email: email,
+      password: password,
+    });
+
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
+  return (
+    <div className="p-7 flex flex-col h-screen justify-between">
+      <div>
+        <img className="w-10 mb-10" src={uberdriverlogo} alt="Uber Logo" />
+        <form
+          onSubmit={(e) => {
+            submitHandler(e);
+          }}
+        >
+          <h3 className="text-lg font-medium mb-2">What's your name?</h3>
+          <div className="flex gap-2">
+            <input
+              className="bg-[#eeeeee] mb-7 rounded px-4 py-2 w-1/2 text-lg placeholder:text-base"
+              type="text"
+              placeholder="First name"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+              required
+            />
+            <input
+              className="bg-[#eeeeee] mb-7 rounded px-4 py-2 w-1/2 text-lg placeholder:text-base"
+              type="text"
+              placeholder="Last name"
+              value={lastName}
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            placeholder="email@gmail.com"
+            required
+          />
+          <h3 className="text-lg font-medium mb-2">Enter your password</h3>
+          <input
+            className="bg-[#eeeeee] mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="password"
+            required
+          />
+          <button
+            className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base"
+            type="submit"
+          >
+            Login
+          </button>
+
+          <p className="text-center">
+            Already have a account?{" "}
+            <Link to="/captain-login" className="text-blue-600">
+              Login here
+            </Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <Link
+          to="/captain-login"
+          className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base"
+        >
+          Sign in as Captain
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CaptainSignup;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <div className="bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1538563188159-070c4db2bc65?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]    h-screen pt-8 flex justify-between flex-col w-full bg-red-400">
+      <div className="bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1538563188159-070c4db2bc65?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]    h-screen pt-8 flex justify-between flex-col w-full">
         <img className="w-10 ml-8" src={uberlogo} alt="Uber Logo" />
         <div className="bg-white pb-7 py-4 px-4">
           <h2 className="text-3xl font-bold ">Get Started with Uber</h2>
